fcbc0a4728d1cec141ed4e0b17095dc234aedc1d
devangmalhotra <malh2226@mylaurier.ca>
2026-03-08 23:04:01 -0400

    updated files

--- Sender/Sender.java
+++ Sender/Sender.java
@@ -8,14 +8,12 @@ public class Sender {
     static int expectedSeqNum = 1;
 
     public static void main(String argv[]) throws Exception {
-        // Creating the datagram socket
         DatagramSocket datagramSocket = null;
         int port = 0;
-        
-        // Getting port
+
         try {
             port = Integer.parseInt(argv[2]);
-            if(port < 1024 || port > 65535) {
+            if (port < 1024 || port > 65535) {
                 System.err.println("Port must be between 1024 and 65535");
                 System.exit(1);
             }
@@ -33,18 +31,16 @@ public class Sender {
 
                 // Sending the SOT packet to start handshake
                 DSPacket packetToSend = new DSPacket(DSPacket.TYPE_SOT, 0, null);
-                byte [] dataBytes = packetToSend.toBytes();
+                byte[] dataBytes = packetToSend.toBytes();
                 DatagramPacket datagramForSot = new DatagramPacket(dataBytes, dataBytes.length, InetAddress.getByName(argv[0]), Integer.parseInt(argv[1]));
 
                 int timeoutTimerHandshake = 0;
 
-                while(!handshakeCompleted) {
+                while (!handshakeCompleted) {
                     datagramSocket.send(datagramForSot);
 
-                    System.out.println("Sender: Sent SOT with seqNum 0. Now waiting for ACK");
-
                     try {
-                        byte[] buffer = new byte[128]; // since each UDP datagram should be 128 bytes
+                        byte[] buffer = new byte[128];
                         DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
                         datagramSocket.receive(dp);
 
@@ -54,110 +50,94 @@ public class Sender {
 
                         if (packetType == DSPacket.TYPE_ACK && packetSeqNum == 0) {
                             handshakeCompleted = true;
-                            System.out.println("Sender: Successful handshake. Received ACK with seqNum 0");
                         }
                     } catch (SocketTimeoutException e) {
                         timeoutTimerHandshake++;
-
-                        if (timeoutTimerHandshake == 3) { //max reached
+                        if (timeoutTimerHandshake == 3) {
                             fis.close();
                             datagramSocket.close();
                             return;
                         }
                     }
-                    
                 }
 
-                // Sending data after handshake has been completed
-                byte[] bufferforFile = new byte[124]; // since each DATA packet carries exactly 124 bytes (except the final one)
+                byte[] bufferforFile = new byte[124];
                 int readBytes;
                 boolean emptyFile = (fis.available() == 0);
                 boolean isGBN = (argv.length >= 6);
 
-            if (!isGBN) { // Stop & Wait
-                if (!emptyFile) {
-                    while((readBytes = fis.read(bufferforFile)) != -1) {
-                        DSPacket packet = new DSPacket(DSPacket.TYPE_DATA, expectedSeqNum, Arrays.copyOf(bufferforFile, readBytes));
-                        byte[] packetBytes = packet.toBytes();
-                        DatagramPacket sendingDatagramPacket = new DatagramPacket(packetBytes, packetBytes.length, InetAddress.getByName(argv[0]), Integer.parseInt(argv[1]));
+                if (!isGBN) { // Stop & Wait
+                    if (!emptyFile) {
+                        while ((readBytes = fis.read(bufferforFile)) != -1) {
+                            DSPacket packet = new DSPacket(DSPacket.TYPE_DATA, expectedSeqNum, Arrays.copyOf(bufferforFile, readBytes));
+                            byte[] packetBytes = packet.toBytes();
+                            DatagramPacket sendingDatagramPacket = new DatagramPacket(packetBytes, packetBytes.length, InetAddress.getByName(argv[0]), Integer.parseInt(argv[1]));
 
-                        System.out.println("Sender: Sent data with seqNum " + expectedSeqNum + ". Now waiting for ACK");
+                            boolean ackReceivedSuccessfully = false;
+                            int timeoutTimer = 0;
 
-                        // Now, wait for ack
-                        boolean ackReceivedSuccessfully = false;
-                        int timeoutTimer = 0;
+                            while (!ackReceivedSuccessfully) {
+                                datagramSocket.send(sendingDatagramPacket);
 
-                        while(!ackReceivedSuccessfully) {
-                            datagramSocket.send(sendingDatagramPacket);
+                                try {
+                                    byte[] bufferForAck = new byte[128];
+                                    DatagramPacket dpForAck = new DatagramPacket(bufferForAck, bufferForAck.length);
+                                    datagramSocket.receive(dpForAck);
+                                    DSPacket packetForAck = new DSPacket(bufferForAck);
 
-                            try {
-                                byte[] bufferForAck = new byte[128];
-                                DatagramPacket dpForAck = new DatagramPacket(bufferForAck, bufferForAck.length);
-                                datagramSocket.receive(dpForAck);
-                                DSPacket packetForAck = new DSPacket(bufferForAck);
-                                
-                                int ackPacketSeqNum = packetForAck.getSeqNum();
-                                byte ackPacketType = packetForAck.getType();
-                                
-                                if(ackPacketSeqNum == expectedSeqNum && ackPacketType == DSPacket.TYPE_ACK) {
-                                    ackReceivedSuccessfully = true;
-                                    expectedSeqNum = (expectedSeqNum + 1) % 128; // since it needs to wrap around
-                                    System.out.println("Receiver: Received ACK with seqNum " + ackPacketSeqNum);
-                                }
-                            } catch (SocketTimeoutException e) {
-                                timeoutTimer++;
+                                    int ackPacketSeqNum = packetForAck.getSeqNum();
+                                    byte ackPacketType = packetForAck.getType();
 
-                                if (timeoutTimer == 3) { //max reached
-                                    fis.close();
-                                    datagramSocket.close();
-                                    return;
+                                    if (ackPacketSeqNum == expectedSeqNum && ackPacketType == DSPacket.TYPE_ACK) {
+                                        ackReceivedSuccessfully = true;
+                                        timeoutTimer = 0;
+                                        expectedSeqNum = (expectedSeqNum + 1) % 128;
+                                    }
+                                } catch (SocketTimeoutException e) {
+                                    timeoutTimer++;
+                                    if (timeoutTimer == 3) {
+                                        fis.close();
+                                        datagramSocket.close();
+                                        return;
+                                    }
                                 }
                             }
-                            
-                        }
-
-                    }
-                }
-
-                // EOT if last file
-                DSPacket packetForEOT = new DSPacket(DSPacket.TYPE_EOT, expectedSeqNum, null);
-                byte[] bytesForEOT = packetForEOT.toBytes();
-
-                DatagramPacket datagramForEOT = new DatagramPacket(bytesForEOT, bytesForEOT.length, InetAddress.getByName(argv[0]), Integer.parseInt(argv[1]));
-
-                boolean eotAckSuccessful = false;
-                int timeoutTimer = 0;
-
-                while(!eotAckSuccessful) {
-                    datagramSocket.send(datagramForEOT);
-
-                    System.out.println("Sender: Sent EOT with seqNum " + expectedSeqNum + ". Now waiting for ACK");
-
-                    try {
-                        byte[] bufferForEOT = new byte[128];
-                        DatagramPacket dpForEOT= new DatagramPacket(bufferForEOT, bufferForEOT.length);
-
-                        datagramSocket.receive(dpForEOT);
-
-                        DSPacket packetForAck = new DSPacket(bufferForEOT);
-                        int ackPacketSeqNum = packetForAck.getSeqNum();
-                        byte ackPacketType = packetForAck.getType();
-
-                        if (ackPacketType == DSPacket.TYPE_ACK && ackPacketSeqNum == expectedSeqNum) {
-                            eotAckSuccessful = true;
-                        }
-                        
-
-                    } catch (SocketTimeoutException e) {
-                        timeoutTimer++;
-
-                        if (timeoutTimer == 3) { //max reached
-                            fis.close();
-                            datagramSocket.close();
-                            return;
                         }
                     }
-                }
+
+                    // Send EOT
+                    DSPacket packetForEOT = new DSPacket(DSPacket.TYPE_EOT, expectedSeqNum, null);
+                    byte[] bytesForEOT = packetForEOT.toBytes();
+                    DatagramPacket datagramForEOT = new DatagramPacket(bytesForEOT, bytesForEOT.length, InetAddress.getByName(argv[0]), Integer.parseInt(argv[1]));
+
+                    boolean eotAckSuccessful = false;
+                    int timeoutTimer = 0;
+
+                    while (!eotAckSuccessful) {
+                        datagramSocket.send(datagramForEOT);
+
+                        try {
+                            byte[] bufferForEOT = new byte[128];
+                            DatagramPacket dpForEOT = new DatagramPacket(bufferForEOT, bufferForEOT.length);
+                            datagramSocket.receive(dpForEOT);
+
+                            DSPacket packetForAck = new DSPacket(bufferForEOT);
+                            int ackPacketSeqNum = packetForAck.getSeqNum();
+                            byte ackPacketType = packetForAck.getType();
+
+                            if (ackPacketType == DSPacket.TYPE_ACK && ackPacketSeqNum == expectedSeqNum) {
+                                eotAckSuccessful = true;
+                            }
+                        } catch (SocketTimeoutException e) {
+                            timeoutTimer++;
+                            if (timeoutTimer == 3) {
+                                fis.close();
+                                datagramSocket.close();
+                                return;
+                            }
+                        }
+                    }
+
                 } else {
                     // GBN
                     int windowSize = Integer.parseInt(argv[5]);
@@ -216,14 +196,19 @@ public class Sender {
                                 int ackedSeq = ackPkt.getSeqNum();
                                 System.out.println("[GBN Sender] Received cumulative ACK seq=" + ackedSeq);
 
-                                int newBase = base;
-                                for (int i = base; i < totalPackets; i++) {
-                                    newBase = i + 1;
-                                    if (allPackets.get(i).getSeqNum() == ackedSeq) break;
-                                }
-                                if (newBase > base) {
-                                    base = newBase;
-                                    timeoutCounter = 0;
+                                int baseSeq = allPackets.get(base).getSeqNum();
+                                int dist = (ackedSeq - baseSeq + 128) % 128;
+
+                                if (dist < windowSize + 1) {
+                                    int newBase = base;
+                                    for (int i = base; i < totalPackets; i++) {
+                                        newBase = i + 1;
+                                        if (allPackets.get(i).getSeqNum() == ackedSeq) break;
+                                    }
+                                    if (newBase > base) {
+                                        base = newBase;
+                                        timeoutCounter = 0;
+                                    }
                                 }
                             }
                         } catch (SocketTimeoutException e) {
@@ -271,6 +256,7 @@ public class Sender {
                         }
                     }
                 }
+
                 fis.close();
                 datagramSocket.close();
 
@@ -284,5 +270,4 @@ public class Sender {
             }
         }
     }
-    
 }
